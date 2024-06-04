77f980550ccee58d823b12c944e830c81056f193
Samuel <velizsamuel50@gmail.com>
2024-06-03 23:39:55 -0400

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://samuelcv-2002.github.io/portafolio/sw.js', { scope: 'https://samuelcv-2002.github.io/portafolio/' })})}
