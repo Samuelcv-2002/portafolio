cfbd89258bee045200a110c58cf77ae6cbb15071
Samuel <velizsamuel50@gmail.com>
2024-07-05 22:50:28 -0400

    feat: component loader

--- src/componentes/loader.jsx
+++ src/componentes/loader.jsx
@@ -1,10 +1,19 @@
+import { useLayoutEffect, useState } from "react";
 
 const Loader = () => {
+    const [isLoading, setIsLoading] = useState(true);
+  
+    useLayoutEffect(() => {
+        setIsLoading(false)
+    }, []);
+  
     return <>
-        <div id="containerLoading">
-            <div className="loader"></div>
-        </div>
-    </>;
-}
+    {isLoading ? <div id="containerLoading">
+        <div className="loader"></div>
+    </div>
+    : null
+    }
+</>;
+  };
 
 export default Loader;
